f080c9aed1626d32f036f8b8410d3969d1c497b2
Yosef Dinerstein <yosefd@microsoft.com>
2012-01-01 00:18:00 +0200

    A few fixes and a new test

--- tests/tests.js
+++ tests/tests.js
@@ -4,63 +4,75 @@ var path = require('path');
 var fs = require('fs');
 
 module.exports = testCase({
-    setUp: function(cb) {
+    setUp: function (cb) {
         this.gitsync = new GitSync();
         this.origin = path.resolve(__dirname, 'testgitrepo');
         this.target = path.join(process.env.TMP || process.env.TMPDIR, Math.round(Math.random() * 100000).toString());
         cb();
     },
-	
-    branches: function(test) {
-    	var self = this;
-    	self.gitsync.branches(self.origin, function(err, branches) {
-    		test.ok(!err, err && err.msg);
-    		test.ok(branches);
-    		test.deepEqual(branches, ['B1', 'B8', 'MyBranch', 'master' ]);
-	        test.done();
-    	});
-    },
-    
-    currentBranch: function(test) {
+
+    branches: function (test) {
         var self = this;
-        self.gitsync.sync(self.origin, 'MyBranch', this.target, function(err) {
+        self.gitsync.branches(self.origin, function (err, branches) {
+            test.ok(!err, err && err.msg);
+            test.ok(branches);
+            test.deepEqual(branches, ['B1', 'B8', 'MyBranch', 'master']);
+            test.done();
+        });
+    },
+
+    currentBranch: function (test) {
+        var self = this;
+        self.gitsync.sync(self.origin, 'MyBranch', this.target, function (err) {
             test.ok(!err, err);
-            self.gitsync.currentBranch(self.target, function(err, branch) {
-               test.equals(branch, "MyBranch");
-               test.done(); 
+            self.gitsync.currentBranch(self.target, function (err, branch) {
+                test.equals(branch, "MyBranch");
+                test.done();
             });
         });
     },
-    
-    syncBranchesClone: function(test) {
-    	var self = this;
-    	self.gitsync.bsyncAll(self.origin, this.target, "PREFIX_", function(err, results) {
-    		test.ok(!err, err && err.msg);
-    		test.ok(results);
-    		for (var b in results) test.ok(!b.err, b.err); // verify we don't have any errors
-    		test.done();
-    	});
-    },
-    
-    remotes: function(test) {
+
+    resync: function (test) {
         var self = this;
-        self.gitsync.sync(self.origin, 'MyBranch', self.target, function(err) {
+        self.gitsync.sync(self.origin, 'MyBranch', self.target, function (err) {
             test.ok(!err, err);
-            self.gitsync.remotes(self.target, function(err, remotes) {
+            // sync again.
+            self.gitsync.sync(self.origin, 'MyBranch', self.target, function (err, results) {
+                test.ok(!err, err);
+                test.done();
+            });
+        });
+    },
+
+    syncBranchesClone: function (test) {
+        var self = this;
+        self.gitsync.bsyncAll(self.origin, this.target, "PREFIX_", function (err, results) {
+            test.ok(!err, err && err.msg);
+            test.ok(results);
+            for (var b in results) test.ok(!b.err, b.err); // verify we don't have any errors
+            test.done();
+        });
+    },
+
+    remotes: function (test) {
+        var self = this;
+        self.gitsync.sync(self.origin, 'MyBranch', self.target, function (err) {
+            test.ok(!err, err);
+            self.gitsync.remotes(self.target, function (err, remotes) {
                 test.ok(!err, err);
                 test.ok(remotes);
                 test.ok(remotes.origin);
                 test.ok(remotes.origin.fetch);
                 test.ok(remotes.origin.push);
-                test.done(); 
+                test.done();
             });
         });
     },
 
-    log: function(test) {
+    log: function (test) {
         var self = this;
-        self.gitsync.sync(self.origin, 'MyBranch', self.target, function(err) {
-            self.gitsync.log(self.target, {}, function(err, commits) {
+        self.gitsync.sync(self.origin, 'MyBranch', self.target, function (err) {
+            self.gitsync.log(self.target, {}, function (err, commits) {
                 console.log(commits);
                 test.ok(!err, JSON.stringify(err));
                 test.equals(commits.length, 4);
@@ -68,14 +80,14 @@ module.exports = testCase({
             });
         });
     },
-    
-    pruneAll: function(test) {
-      var self = this;
-      self.gitsync.bsyncAll(self.origin, self.target, "PREFIX_", function(err, results) {
-        self.gitsync.pruneAll(self.target, function(err) {
-          test.ok(!err, err);
-          test.done();
+
+    pruneAll: function (test) {
+        var self = this;
+        self.gitsync.bsyncAll(self.origin, self.target, "PREFIX_", function (err, results) {
+            self.gitsync.pruneAll(self.target, function (err) {
+                test.ok(!err, err);
+                test.done();
+            });
         });
-      });
     }
 });
